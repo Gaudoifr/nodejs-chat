e48e0dcf4d340b78874bcdee332b5b2200a1c39b
Gaudoifr <francois.gaudoin@gmail.com>
2020-08-27 12:18:30 +0200

    first raw before optimization

---
+++ app.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const fs = require('fs');
+
+// Chargement du fichier index.html affiché au client
+const server = http.createServer(function(req, res) {
+    fs.readFile('./index.html', 'utf-8', function(error, content) {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(content);
+    });
+});
+
+// Chargement de socket.io
+const io = require('socket.io').listen(server);
+
+// Chargement de ent : évite les caractères HTML
+const ent = require ('ent');
+
+// Quand un client se connecte, on le note dans la console
+io.sockets.on('connection', function (socket, pseudo) {
+    // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
+    socket.on('nouveau_client', function(pseudo) {
+        pseudo = ent.encode(pseudo);
+        socket.pseudo = pseudo;
+        socket.broadcast.emit('nouveau_client', pseudo);
+    });
+
+    // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
+    socket.on('message', function (message) {
+        message = ent.encode(message);
+        socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
+    });
+
+});server.listen(8080);
